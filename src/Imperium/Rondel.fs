75cb290b2eeb0cd47160ae7567009fd8da8ea1a4
Bartul Bonacic <bartul.bonacic@live.com>
2019-12-06 11:18:36 +0100

    Small white space removal

--- src/Imperium/Rondel.fs
+++ src/Imperium/Rondel.fs
@@ -8,7 +8,7 @@ module Rondel =
     type Position = Investor | Import | Production1 | Maneuver1 | Taxation | Factory | Production2 | Maneuver2
     type NationPosition = { Nation : Nation; Position : Position }
         
-    let move    (nationPosition : NationPosition) (nationPowerFactor : int) (position : Position) (payment : Payment) =
+    let move (nationPosition : NationPosition) (nationPowerFactor : int) (position : Position) (payment : Payment) =
         if nationPowerFactor = 1 then
             Ok nationPosition
         else
