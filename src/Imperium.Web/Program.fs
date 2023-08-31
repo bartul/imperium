970d88315d0f7eebc08ab9ea7a3f74da1d636726
Bartul Bonacic <bartul.bonacic@live.com>
2023-08-31 17:50:40 +0200

    Remove azure sdk and open telemetry distro instead

--- src/Imperium.Web/Program.fs
+++ src/Imperium.Web/Program.fs
@@ -1,16 +1,23 @@
 open System
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
+open Azure.Monitor.OpenTelemetry.AspNetCore
 open Microsoft.Extensions.DependencyInjection
-
+open Microsoft.Extensions.Logging
 
 [<EntryPoint>]
 let main args =
     let builder = WebApplication.CreateBuilder(args)
+    builder.Services.AddOpenTelemetry().UseAzureMonitor() |> ignore
+    
     let app = builder.Build()
-    builder.Services.AddApplicationInsightsTelemetry() |> ignore
+    
+    app.Services.GetService<IHostApplicationLifetime>()
+        .ApplicationStarted
+        .Register(fun () -> app.Services.GetService<ILogger>().LogInformation("Web application Imperium started") |> ignore) |> ignore  
 
     app.MapGet("/", Func<string>(fun () -> "Hello Imperium!")) |> ignore
+
 
     app.Run()
 
