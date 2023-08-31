11afe0f2e18f8e453d6b7db5c47e56d9ef712721
Bartul Bonacic <bartul.bonacic@live.com>
2023-08-31 15:00:35 +0200

    Add application insights telemetry

--- src/Imperium.Web/Program.fs
+++ src/Imperium.Web/Program.fs
@@ -1,11 +1,14 @@
 open System
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.DependencyInjection
+
 
 [<EntryPoint>]
 let main args =
     let builder = WebApplication.CreateBuilder(args)
     let app = builder.Build()
+    builder.Services.AddApplicationInsightsTelemetry() |> ignore
 
     app.MapGet("/", Func<string>(fun () -> "Hello Imperium!")) |> ignore
 
