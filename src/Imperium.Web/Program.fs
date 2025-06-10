41508a9bb6799bf2e0785f7964606e3559e6b4f2
Bartul Bonacic <bartul.bonacic@live.com>
2025-06-10 21:22:39 +0200

    Update to dotnet 9

--- src/Imperium.Web/Program.fs
+++ src/Imperium.Web/Program.fs
@@ -1,25 +1,24 @@
 open System
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
-open Azure.Monitor.OpenTelemetry.AspNetCore
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Logging
 
 [<EntryPoint>]
 let main args =
     let builder = WebApplication.CreateBuilder(args)
-    builder.Services.AddOpenTelemetry().UseAzureMonitor() |> ignore
+    builder.Logging.AddConsole() |> ignore
     
     let app = builder.Build()
     
-    app.Services.GetService<IHostApplicationLifetime>()
+    let logger = app.Services.GetRequiredService<ILogger<obj>>()
+    app.Services.GetRequiredService<IHostApplicationLifetime>()
         .ApplicationStarted
-        .Register(fun () -> app.Services.GetService<ILogger>().LogInformation("Web application Imperium started") |> ignore) |> ignore  
+        .Register(fun () -> logger.LogInformation("Web application Imperium started")) |> ignore
 
     app.MapGet("/", Func<string>(fun () -> "Hello Imperium!")) |> ignore
-
-
+    
     app.Run()
-
+    
     0 // Exit code
 
