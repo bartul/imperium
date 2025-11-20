6b501650995ae85c3102d5f4deb338f04cba3bbb
Bartul Bonacic <bartul.bonacic@live.com>
2025-11-20 20:15:25 +0100

    Update to dotnet 10

--- src/Imperium/Rondel.fs
+++ src/Imperium/Rondel.fs
@@ -5,12 +5,5 @@ open FinancialSystem
 
 module Rondel =
 
-    type Position = Investor | Import | Production1 | Maneuver1 | Taxation | Factory | Production2 | Maneuver2
-    type NationPosition = { Nation : Nation; Position : Position }
-        
-    let move (nationPosition : NationPosition) (nationPowerFactor : int) (position : Position) (payment : Payment) =
-        if nationPowerFactor = 1 then
-            Ok nationPosition
-        else
-            Error ""
+   let x = 3
      
